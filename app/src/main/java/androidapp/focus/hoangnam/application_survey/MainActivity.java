403ebfa114ba295b2729862ba9d09483d0a623df
nguyenvannam19942802 <nguyenvannam19942802@gmail.com>
2017-12-23 14:14:12 +0700

    #Update Setting

--- app/src/main/java/androidapp/focus/hoangnam/application_survey/MainActivity.java
+++ app/src/main/java/androidapp/focus/hoangnam/application_survey/MainActivity.java
@@ -3,6 +3,9 @@ package androidapp.focus.hoangnam.application_survey;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -12,6 +15,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.tab_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId())
+        {
+            case R.id.language_en:
+                // chuyển toàn bộ sang tiếng anh
+                break;
+            case R.id.language_vn:
+                 // chuyển toàn bộ sang tiếng việt
+                break;
+            case R.id.change_background:
+
+                // hiển thị popup chọn ngôn ngữ
+                break;
+        }
+        return true;
     }
 
 
