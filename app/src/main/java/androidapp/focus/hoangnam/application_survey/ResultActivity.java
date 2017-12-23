cc2a879fdc48c46ea1d623359d6651396e8b997b
nguyenvannam19942802 <nguyenvannam19942802@gmail.com>
2017-12-23 13:39:27 +0700

    #Add  Khởi tạo giao diện cho ứng dụng survey

---
+++ app/src/main/java/androidapp/focus/hoangnam/application_survey/ResultActivity.java
@@ -0,0 +1,17 @@
+package androidapp.focus.hoangnam.application_survey;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by HoangNam on 12/23/2017.
+ */
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
