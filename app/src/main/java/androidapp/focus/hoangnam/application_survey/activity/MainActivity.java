61ecce514aa74f464a19f8ba7a80d046fede4464
ongdo2004@gmail.com <ongdo2004@gmail.com>
2017-12-24 09:29:42 +0700

    #update: - survey: implement logic;

---
+++ app/src/main/java/androidapp/focus/hoangnam/application_survey/activity/MainActivity.java
@@ -0,0 +1,41 @@
+package androidapp.focus.hoangnam.application_survey.activity;
+
+import android.os.Bundle;
+import android.widget.RadioButton;
+
+import java.util.Arrays;
+import java.util.List;
+
+import androidapp.focus.hoangnam.application_survey.R;
+
+public class MainActivity extends BaseQuestionActivity {
+
+    List<RadioButton> radioButtons;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        radioButtons = Arrays.asList(
+                (RadioButton) findViewById(R.id.radioButton),
+                (RadioButton) findViewById(R.id.radioButton2),
+                (RadioButton) findViewById(R.id.radioButton3),
+                (RadioButton) findViewById(R.id.radioButton4),
+                (RadioButton) findViewById(R.id.radioButton5)
+        );
+    }
+
+    @Override
+    protected Object getAnswerData() {
+
+        for (RadioButton radioButton : radioButtons) {
+
+            if (radioButton.isChecked()) return radioButton.getText();
+        }
+
+        return null;
+    }
+}
